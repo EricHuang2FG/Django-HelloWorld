dc3950ded7e5151dbf80e320a17675ae0df2c8ba
EricHuang2FG <xilahuade@foxmail.com>
2024-03-12 23:22:31 -0400

    Updated files

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def homePageView(request):
-    return HttpResponse("Testing, hello!")
+    time = datetime.now().strftime('%Y/%m/%d %H: %M: %S')
+    return HttpResponse("Testing, hello! The current time is: " + time)
